153d327fdde04e4a67cfd6610e551d9d9f8d9875
Christina Repou <crepou@3-F-7.42heilbronn.de>
2023-09-16 13:29:21 +0200

    checking if input given is valid

--- header_files/cub3d_errors.h
+++ header_files/cub3d_errors.h
@@ -1,23 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   cub3d_errors.h                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: crepou <crepou@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/09/14 17:29:46 by crepou            #+#    #+#             */
-/*   Updated: 2023/09/16 12:51:21 by crepou           ###   ########.fr       */
+/*   Created: 2023/09/16 12:04:01 by crepou            #+#    #+#             */
+/*   Updated: 2023/09/16 12:49:57 by crepou           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../header_files/cub3d.h"
+#ifndef CUB3D_ERRORS_H
+# define CUB3D_ERRORS_H
 
-int	main(int argc, char *argv[])
-{
-	int	fd;
+/************** input_check.c **************/
+int	check_extension(const char *filename);
+int	check_number_of_arguments(int argc);
+int	check_if_file_exists(int fd, const char *filename);
+int	get_file_descriptor(const char *filename);
+int	is_input_valid(int argc, char *argv[]);
 
-	fd = is_input_valid(argc, argv);
-	if (!fd)
-		return (-1);
-	return (0);
-}
+#endif
