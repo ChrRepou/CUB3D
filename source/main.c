b1e8667bf1ca91092c278865037f9efcc82e65b5
tmarts <tmarts@student.42heilbronn.de>
2023-11-09 20:31:24 +0100

    only commenting uncommenting

--- source/main.c
+++ source/main.c
@@ -6,7 +6,7 @@
 /*   By: tmarts <tmarts@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/14 17:29:46 by crepou            #+#    #+#             */
-/*   Updated: 2023/11/09 19:45:57 by tmarts           ###   ########.fr       */
+/*   Updated: 2023/11/09 20:26:07 by tmarts           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ int	main(int argc, char *argv[])
 	t_caster	cast_data;
 	t_minimap	minimap;
 
-	atexit(leaks);
+	// atexit(leaks);
 	fd = is_input_valid(argc, argv);
 	if (!fd)
 		return (-1);
